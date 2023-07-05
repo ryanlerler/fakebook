b6d3847fa622c310e2ec6c7c6d1250c27542be90
kaisheng1989 <kaisheng1989@gmail.com>
2023-07-05 18:08:39 +0800

    Separation of Sign up and login form

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,56 +1,30 @@
 import React, { useState } from "react";
-import "./LoginForm.css";
-import { Button } from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import { auth } from "../firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
+import { Button, Card } from "react-bootstrap";
+import { auth } from "./firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function LoginForm() {
+  // Setting State
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [message, setMessage] = useState("");
 
-  // Sign up function
-  const signUp = () => {
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        console.log(user);
-        navigate("/threads");
-        // ...
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        console.log(errorCode);
-        const errorMessage = error.message;
-        console.log(errorMessage);
-        if (errorCode === "auth/invalid-email") {
-          setMessage("Please Key in a Valid Email Address.");
-        } else if (errorCode === "auth/email-already-in-use") {
-          setMessage("Email in use. Kindly pick another email ");
-        } else if (errorCode === "auth/wrong-password") {
-          setMessage("Incorrect Password. Try again!");
-        } else if (errorCode === "auth/weak-password") {
-          setMessage("Please key in a password of at least 6 characters.");
-        } else {
-          return setMessage("");
-        }
-        // ..
-      });
+  // see the current state of the email
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
   };
-// Sign In Function
+
+  // see the current state of the password
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  // sign in and catching the error from firebase to reflect the message
   const signIn = () => {
     signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
@@ -78,66 +52,49 @@ function LoginForm() {
         }
       });
   };
-
   return (
-    <div className="Login">
-      <Container>
-        <Row>
-          <Col>
-            <div className="Fakebook">
-              <div className="Fakebooktext align-left text-primary fs-1 fw-bold">
-                Fakebook
-              </div>
-              <div className="title fs-2">
-                Connect with friends and the world <br /> around you on
-                Fakebook.
-              </div>
-            </div>
-          </Col>
-          <Col>
-            <div className="logincontainer border border-top-0 border-white mx-auto p-5 border-opacity-25">
-              <div className="logindetail">
-                <Form>
-                  <Form.Group className="mb-3">
-                    <Form.Label className="fs-5">Email</Form.Label>
-                    <br />
-                    <Form.Control
-                      value={email}
-                      name="email"
-                      type="email"
-                      placeholder="Email address"
-                      onChange={(e) => setEmail(e.target.value)}
-                    />
-                  </Form.Group>
-                  <br />
-                  <Form.Group className="mb-3">
-                    <Form.Label className="fs-5">Password</Form.Label>
-                    <br />
-                    <Form.Control
-                      value={password}
-                      name="password"
-                      type="password"
-                      placeholder="Password"
-                      onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <p className="text-danger">{message}</p>
-                  </Form.Group>
-                </Form>
-              </div>
-              <br />
-              <div className="d-flex justify-content-evenly">
-                <Button onClick={signUp} className="btn btn-primary btn-sm">
-                  Sign Up
-                </Button>
-                <Button onClick={signIn} className="btn btn-primary btn-sm">
+    <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Log in </h2>
+              <Form>
+                <Form.Group id="email">
+                  <Form.Label className="fs-5">Email:</Form.Label>
+                  <Form.Control
+                    value={email}
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email Address"
+                    onChange={handleEmail}
+                  />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label className="fs-5">Password:</Form.Label>
+                  <Form.Control
+                    value={password}
+                    name="password"
+                    type="password"
+                    placeholder="Enter Password"
+                    onChange={handlePassword}
+                  />
+                </Form.Group>
+                <br />
+                <p className="text-danger">{message}</p>
+                <br />
+                <Button className="w-100" onClick={signIn}>
                   Sign In
                 </Button>
-              </div>
-            </div>
-          </Col>
-        </Row>
+              </Form>
+            </Card.Body>
+          </Card>
+        </div>
       </Container>
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/SignUpForm.js
@@ -0,0 +1,168 @@
+import React, { useState } from "react";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import { Button, Card } from "react-bootstrap";
+import { auth } from "./firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function SignUpForm() {
+  // Setting State
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordconfirmation, setPasswordConfirmation] = useState("");
+  const [emailmessage, setEmailMessage] = useState("");
+  const [usernamemessage, setUsernameMessage] = useState("");
+  const [passwordmessage, setPasswordMessage] = useState("");
+
+  // Handle Email change
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+  console.log(email);
+
+  const checkEmailValidation = () => {
+    const RGEXPemail =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (RGEXPemail.test(email)) {
+      setEmailMessage("Email is Valid");
+    } else if (email === "") {
+      setEmailMessage("Please Enter Email");
+    } else if (!RGEXPemail.test(email)) {
+      setEmailMessage("Email is not valid");
+    } else {
+      setEmailMessage("");
+    }
+  };
+
+  const handleUserNameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const checkUsernameValidation = () => {
+    if (username === "") {
+      setUsernameMessage("Please Key in username");
+    } else {
+      setUsernameMessage("Valid Username");
+    }
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+  const handleConfirmationPassword = (e) => {
+    setPasswordConfirmation(e.target.value);
+  };
+
+  const checkPasswordValidation = () => {
+    if (password === passwordconfirmation) {
+      setPasswordMessage("Password matched!");
+    } else {
+      setPasswordMessage("Password does not match");
+    }
+  };
+
+  const signUp = () => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/threads");
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        console.log(errorCode);
+        const errorMessage = error.message;
+        console.log(errorMessage);
+      });
+  };
+
+  return (
+    <>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Card>
+            <Card.Body>
+              <h2 className="text-center mb-4">Sign Up </h2>
+              <Form>
+                <Form.Group id="username">
+                  <Form.Label className="fs-5">Username:</Form.Label>
+                  <Form.Control
+                    value={username}
+                    name="username"
+                    type="text"
+                    placeholder="Enter Username"
+                    onChange={handleUserNameChange}
+                  />
+                  <p className="fs-6 text-danger">{usernamemessage}</p>
+                </Form.Group>
+
+                <Form.Group id="email">
+                  <Form.Label className="fs-5">Email:</Form.Label>
+                  <Form.Control
+                    value={email}
+                    name="email"
+                    type="email"
+                    placeholder="Enter Email Address"
+                    onChange={handleEmailChange}
+                  />
+                  <p className="fs-6 text-danger">{emailmessage}</p>
+                </Form.Group>
+
+                <Form.Group id="password">
+                  <Form.Label className="fs-5">Password:</Form.Label>
+                  <Form.Control
+                    value={password}
+                    name="password"
+                    type="password"
+                    placeholder="Password"
+                    onChange={handlePassword}
+                  />
+
+                  <Form.Group id="confirmpassword">
+                    <Form.Label className="fs-5">
+                      Password Confirmation
+                    </Form.Label>
+                    <Form.Control
+                      value={passwordconfirmation}
+                      name="password"
+                      type="password"
+                      placeholder="Password"
+                      onChange={handleConfirmationPassword}
+                    />
+                    <p className="fs-6 text-danger">{passwordmessage}</p>
+                  </Form.Group>
+                </Form.Group>
+                <br />
+                <Button
+                  onClick={() => {
+                    checkEmailValidation();
+                    checkUsernameValidation();
+                    checkPasswordValidation();
+                    signUp();
+                  }}
+                  className="w-100"
+                >
+                  Sign Up
+                </Button>
+              </Form>
+            </Card.Body>
+          </Card>
+          <br />
+          <div className="w-100 text-center mt2 fs-5">
+            Already have an account?
+            <Button onClick={navigate("/Login")}>Log In </Button>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default SignUpForm;
