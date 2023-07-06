a65da2fabc280914fe0586a2f8047172555107dd
kaisheng1989 <kaisheng1989@gmail.com>
2023-07-06 09:51:53 +0800

    changes to Login page

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -6,6 +6,8 @@ import { auth } from "../firebase";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function LoginForm() {
   // Setting State
@@ -58,40 +60,62 @@ function LoginForm() {
         className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
-        <div className="w-100" style={{ maxWidth: "400px" }}>
-          <Card>
-            <Card.Body>
-              <h2 className="text-center mb-4">Log in </h2>
-              <Form>
-                <Form.Group id="email">
-                  <Form.Label className="fs-5">Email:</Form.Label>
-                  <Form.Control
-                    value={email}
-                    name="email"
-                    type="email"
-                    placeholder="Enter Email Address"
-                    onChange={handleEmail}
-                  />
-                </Form.Group>
-                <Form.Group id="password">
-                  <Form.Label className="fs-5">Password:</Form.Label>
-                  <Form.Control
-                    value={password}
-                    name="password"
-                    type="password"
-                    placeholder="Enter Password"
-                    onChange={handlePassword}
-                  />
-                </Form.Group>
-                <br />
-                <p className="text-danger">{message}</p>
-                <br />
-                <Button className="w-100" onClick={signIn}>
-                  Sign In
-                </Button>
-              </Form>
-            </Card.Body>
-          </Card>
+        <div className="w-100" style={{ maxWidth: "800px" }}>
+          <Row>
+            <Col>
+              <Card className="bg-transparent text-body mt-4">
+                <Card.Body>
+                  <h2 className="text-center mb-5  text-primary">FakeBook</h2>
+                  <div className="fs-2">
+                    <p className="text-white">
+                      Connect with people and the world <br /> around you on
+                      Fakebook.
+                    </p>
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col>
+              <Card>
+                <Card.Body>
+                  <h2 className="text-center mb-4">Log in </h2>
+                  <Form>
+                    <Form.Group id="email">
+                      <Form.Label className="fs-5">Email:</Form.Label>
+                      <Form.Control
+                        value={email}
+                        name="email"
+                        type="email"
+                        placeholder="Enter Email Address"
+                        onChange={handleEmail}
+                      />
+                    </Form.Group>
+                    <Form.Group id="password">
+                      <Form.Label className="fs-5">Password:</Form.Label>
+                      <Form.Control
+                        value={password}
+                        name="password"
+                        type="password"
+                        placeholder="Enter Password"
+                        onChange={handlePassword}
+                      />
+                    </Form.Group>
+                   
+                    <p className="text-danger">{message}</p>
+                    
+                    <Button className="w-100" onClick={signIn}>
+                      Sign In
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+              <br/>
+              <div className="w-100 text-center mt2 fs-5">
+                Not yet a Member?
+                <Button onClick={() => navigate("/signup")}>Sign Up </Button>
+              </div>
+            </Col>
+          </Row>
         </div>
       </Container>
     </>
